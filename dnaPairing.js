ba20445081d47d799cab06f853f8de6557b102e7
bambi <bambi@anniecannons.com>
2019-09-24 10:45:56 -0700

    The completed solution for dnaPairing exercise.

--- dnaPairing.js
+++ dnaPairing.js
@@ -10,12 +10,32 @@
 
 //The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
-function pairElement(str) {
-  //your code here
-}
+//Input: character strand/string, Output: nested array
+//create 2 variables, one that stores the transformation of the input string into an array. create  a second variable that stores an empty array/our output.
+//create a for loop that runs through the array
+//create a conditional statement that tests if the current letter we are on is equal to one of the 4 provided letters
+//if TRUE, push an array with the 2 strings into our variable storing the array.
+//tools: split method, for loop, conditional statement,
 
-pairElement("GCG");
 
+function pairElement(str){
+  var matchingPairs = [];
+  var newArray = str.split("");
+  for(var i = 0; i < newArray.length; i++){
+    if (newArray[i] === "G"){
+      matchingPairs.push(["G","C"]);
+    }else if(newArray[i] === "C"){
+      matchingPairs.push(["C", "G"]);
+    }else if(newArray === "A"){
+      matchingPairs.push(["A", "T"]);
+    }else{
+      matchingPairs.push(["T", "A"]);
+    }
+  }
+  return matchingPairs;
+ }
+ console.log(pairElement("GCGTA"));
+ 
 //TEST CASES
 //pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
 //pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
